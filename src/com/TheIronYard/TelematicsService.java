749030b2fe6482e1913044dc8c610ee4109cae17
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-09-16 14:54:14 -0500

    start TelematicService method build

--- src/com/TheIronYard/TelematicsService.java
+++ src/com/TheIronYard/TelematicsService.java
@@ -1,5 +1,15 @@
 package com.TheIronYard;
 
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+
 public class TelematicsService {
 
 
@@ -8,16 +18,23 @@ public class TelematicsService {
 
     void report(VehicleInfo vehicleInfo){
 
-
+        ObjectMapper mapper = new ObjectMapper();
         try {
-            File file = new File("greetings.txt");
-            FileWriter fileWriter = new FileWriter(file);
-            fileWriter.write("Hello World!\n"); //Very simple!
-            fileWriter.write("File IO is cool and not scary!");
-            fileWriter.close(); //close() cleans up and commits changes
-        } //If Java doesn't find the file it will create it for us
-        catch (IOException ex) { //A general exception that covers many errors
-            ex.printStackTrace();
+            String json = mapper.writeValueAsString(vehicleInfo);
+
+
+            try {
+                File file = new File(Integer.toString(vehicleInfo.getVIN()) + ".json");
+                FileWriter fileWriter = new FileWriter(file);
+                fileWriter.write("Hello World!\n"); //Very simple!
+                fileWriter.write("File IO is cool and not scary!");
+                fileWriter.close(); //close() cleans up and commits changes
+            } //If Java doesn't find the file it will create it for us
+            catch (IOException ex) { //A general exception that covers many errors
+                ex.printStackTrace();
+            }
+        } catch(JsonProcessingException e){
+            e.printStackTrace();
         }
 
     }
